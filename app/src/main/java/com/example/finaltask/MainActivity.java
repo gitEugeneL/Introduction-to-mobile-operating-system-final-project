72a8a0eb15501bee111451a37ba64ce6da088732
Eugene L <euglihon.dev@gmail.com>
2024-05-16 18:46:00 +0200

    added base logic

--- app/src/main/java/com/example/finaltask/MainActivity.java
+++ app/src/main/java/com/example/finaltask/MainActivity.java
@@ -1,65 +1,131 @@
 package com.example.finaltask;
 
+import static com.example.finaltask.Maze.*;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int[][] data;
+    private String binCurrent;
+    private int[] position;
+    private int start, finish, current, left, right, up, down;
+    Button buttonTop, buttonBottom, buttonLeft, buttonRight;
+    TextView mainText;
 
-    private int[] maze;
-    private int currentNumber;
+    private void update() {
+        assert position != null;
+        current = data[position[0]][position[1]];
+        binCurrent = Maze.getBinaryValue(current);
+        left = Maze.findLeft(data, position);
+        right = Maze.findRight(data, position);
+        up = Maze.findUp(data, position);
+        down = Maze.findDown(data, position);
+    }
 
+    @SuppressLint("SetTextI18n")
+    private void updateUI() {
+        mainText.setText(current + "\n" + binCurrent);
+        buttonLeft.setText(left != -1 ? left + "\n" + getBinaryValue(left) : "wall");
+        buttonRight.setText(right != -1 ? right + "\n" + getBinaryValue(right) : "wall");
+        buttonBottom.setText(down != -1 ? down + "\n" + getBinaryValue(down) : "wall");
+        buttonTop.setText(up != -1 ? up + "\n" + getBinaryValue(up) : "wall");
 
+        buttonLeft.setBackgroundColor(Color.BLUE);
+        buttonRight.setBackgroundColor(Color.BLUE);
+        buttonTop.setBackgroundColor(Color.BLUE);
+        buttonBottom.setBackgroundColor(Color.BLUE);
+
+        if (binCurrent.charAt(3) != '1' || left == -1)
+            buttonLeft.setBackgroundColor(Color.GRAY);
+
+        if (binCurrent.charAt(2) != '1' || right == -1)
+            buttonRight.setBackgroundColor(Color.GRAY);
+
+        if (binCurrent.charAt(1) != '1' || up == -1)
+            buttonTop.setBackgroundColor(Color.GRAY);
+
+        if (binCurrent.charAt(0) != '1' || down == -1)
+            buttonBottom.setBackgroundColor(Color.GRAY);
+    }
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        maze = Maze.getData();
-        currentNumber = maze[maze.length -1];
+        buttonTop = findViewById(R.id.buttonTop);
+        buttonBottom = findViewById(R.id.buttonBottom);
+        buttonLeft = findViewById(R.id.buttonLeft);
+        buttonRight = findViewById(R.id.buttonRight);
+        mainText = findViewById(R.id.textViewCenter);
 
-        Button buttonTop = findViewById(R.id.buttonTop);
-        Button buttonBottom = findViewById(R.id.buttonBottom);
-        Button buttonLeft = findViewById(R.id.buttonLeft);
-        Button buttonRight = findViewById(R.id.buttonRight);
-        TextView mainText = findViewById(R.id.textViewCenter);
+        data = getData();
+        start = getStart(data);
+        finish = getFinish(data);
 
-        buttonTop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // todo
+        current = start;
+        position = Maze.findInitPosition(data, current);
+
+        update();
+        updateUI();
+
+        buttonLeft.setOnClickListener(v -> {
+            if (binCurrent.charAt(3) != '1') {
+                // todo you lost
+                return;
+            }
+
+            if (position[1] > 0) {
+                position[1]--;
+                update();
+                updateUI();
             }
         });
 
-        buttonBottom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // todo
+        buttonRight.setOnClickListener(v -> {
+            if (binCurrent.charAt(2) != '1') {
+                // todo you lost
+                return;
+            }
+
+            if (position[1] < data.length - 1) {
+                position[1]++;
+                update();
+                updateUI();
             }
         });
 
-        buttonLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // todo
+        buttonTop.setOnClickListener(v -> {
+            if (binCurrent.charAt(1) != '1') {
+                // todo you lost
+                return;
+            }
+
+            if (position[0] > 0) {
+                position[0]--;
+                update();
+                updateUI();
             }
         });
 
-        buttonRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // todo
+        buttonBottom.setOnClickListener(v -> {
+            if (binCurrent.charAt(0) != '1') {
+                // todo you lost
+                return;
             }
-        });
 
-        mainText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // todo
+            if (position[0] < data.length - 1) {
+                position[0]++;
+                update();
+                updateUI();
             }
         });
     }
--- app/src/main/java/com/example/finaltask/Maze.java
+++ app/src/main/java/com/example/finaltask/Maze.java
@@ -1,23 +1,74 @@
 package com.example.finaltask;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Maze {
-    private static final int[] data = { 10, 8, 10, 9, 28, 1, 0, 12, 12, 10, 9, 13, 6, 5, 6, 5};
+    private static final int[][] baseData = {
+            {10, 8, 10, 9},
+            {28, 1, 0, 12},
+            {12, 10, 9, 13},
+            {6, 5, 6, 5}
+    };
 
-    public static int[] getData() {
-        Set<Integer> uniqueNumbers = new HashSet<>();
-        for (int num : data)
-            uniqueNumbers.add(num);
+    public static int[][] getData() {
+        return baseData;
+    }
 
-        int[] sortedData = new int[uniqueNumbers.size()];
-        int i = 0;
-        for (int num : uniqueNumbers)
-            sortedData[i++] = num;
+    public static int getStart(int[][] data){
+        return Arrays.stream(data)
+                .flatMapToInt(Arrays::stream)
+                .max()
+                .orElseThrow(() -> new RuntimeException("array is empty"));
+    }
 
-        Arrays.sort(sortedData);
-        return sortedData;
+    public static int getFinish(int[][] data){
+        return Arrays.stream((data))
+                .flatMapToInt(Arrays::stream)
+                .min()
+                .orElseThrow(() -> new RuntimeException("array is empty"));
+    }
+
+    public static String getBinaryValue(int number) {
+        return String
+                .format("%4s", Integer.toBinaryString(number))
+                .replace(' ', '0')
+                .substring(Math.max(0, Integer.toBinaryString(number).length() - 4));
+    }
+
+    public static int[] findInitPosition(int[][] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++)
+                if (array[i][j] == target)
+                    return new int[]{i, j};
+        }
+        return null;
+    }
+
+    public static int findLeft(int[][] array, int[] position) {
+        if (position[1] > 0)
+            return array[position[0]][position[1] - 1];
+        else
+            return -1;
+    }
+
+    public static int findRight(int[][] array, int[] position) {
+        if (position[1] < array[0].length - 1)
+            return array[position[0]][position[1] + 1];
+        else
+            return -1;
+    }
+
+    public static int findDown(int[][] array, int[] position) {
+        if (position[0] < array.length - 1)
+            return array[position[0] + 1][position[1]];
+        else
+            return -1;
+    }
+
+    public static int findUp(int[][] array, int[] position) {
+        if (position[0] > 0)
+            return array[position[0] - 1][position[1]];
+        else
+            return -1;
     }
 }
